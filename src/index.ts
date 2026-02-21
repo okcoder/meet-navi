4b3845fe9ca6a1dbb9a2821b11c8d4fb59fafb59
Hong WANG <okcoder@live.com>
2026-02-21 15:56:02 +0900

    ``` アプリを起動したら、https://myaccount.google.com/画面を表示するように修正してください。 ```

--- src/index.ts
+++ src/index.ts
@@ -20,8 +20,8 @@ const createWindow = (): void => {
     },
   });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  // and load the requested page.
+  mainWindow.loadURL('https://myaccount.google.com/');
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
